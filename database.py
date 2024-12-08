1ad653e79bb3c2291372d8159418f5971ddcc781
Асанали Битанов <asan@MacBook-Air-Asanali.local>
2024-12-08 21:38:56 +0500

    поменял базу на postgresql и начал деплой на heroku

--- database.py
+++ database.py
@@ -1,35 +1,42 @@
-import sqlite3
+import os
+import psycopg2
 
-# Create table if it doesn't exist
+# Получение соединения с базой данных
+def get_database_connection():
+    database_url = os.getenv("DATABASE_URL")  
+    return psycopg2.connect(database_url, sslmode='require')
+
 def initialize_database():
-    conn = sqlite3.connect('data/people.db')
+    conn = get_database_connection()
     cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS people
-                  (iin TEXT PRIMARY KEY,
-                   has_debt BOOLEAN,
-                   health_issues BOOLEAN,
-                   mentally_unstable BOOLEAN,
-                   drug_use BOOLEAN,
-                   alcohol_use BOOLEAN,
-                   description TEXT)''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS people (
+            iin TEXT PRIMARY KEY,
+            has_debt BOOLEAN,
+            health_issues BOOLEAN,
+            mentally_unstable BOOLEAN,
+            drug_use BOOLEAN,
+            alcohol_use BOOLEAN,
+            description TEXT
+        )
+    ''')
     conn.commit()
     conn.close()
 
-# Function to add a person to the database
 def add_person_to_db(iin, data):
-    conn = sqlite3.connect('data/people.db')
+    conn = get_database_connection()
     cursor = conn.cursor()
-    cursor.execute('''INSERT INTO people (iin, has_debt, health_issues, mentally_unstable, drug_use, alcohol_use, description)
-                      VALUES (?, ?, ?, ?, ?, ?, ?)''',
-                   (iin, data['has_debt'], data['health_issues'], data['mentally_unstable'], data['drug_use'], data['alcohol_use'], data['description']))
+    cursor.execute('''
+        INSERT INTO people (iin, has_debt, health_issues, mentally_unstable, drug_use, alcohol_use, description)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ''', (iin, data['has_debt'], data['health_issues'], data['mentally_unstable'], data['drug_use'], data['alcohol_use'], data['description']))
     conn.commit()
     conn.close()
 
-# Function to find a person by ИИН (ID)
 def find_person_by_iin(iin):
-    conn = sqlite3.connect('data/people.db')
+    conn = get_database_connection()
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM people WHERE iin = ?', (iin,))
+    cursor.execute('SELECT * FROM people WHERE iin = %s', (iin,))
     person = cursor.fetchone()
     conn.close()
-    return person  # Returns None if not found
+    return person
